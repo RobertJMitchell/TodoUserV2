a585b1c64ffd9cfdddc85815ed01aeaaf096f6c8
RobertJMitchell <rjrocks32@gmail.com>
2015-04-02 13:46:47 -0500

    updated web.config

---
+++ TodoUserV2.Data/ApplicationDbContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TodoUserV2.Data.Model;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace TodoUserV2.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext()
+            : base("TodoUserV2Connection", throwIfV1Schema: false)
+        {
+        }
+
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
+    }
+}
