2211b9eb63f7b26693101e41abe4dbeea8cf5ff9
RobertJMitchell <rjrocks32@gmail.com>
2015-04-02 13:36:21 -0500

    base package

---
+++ TodoUserV2.Data.Model/Todo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TodoUserV2.Data.Model
+{
+    public class Todo
+    {
+        public int TodoId { get; set; }
+        public string Task { get; set; }
+        
+        public int UserId { get; set; }
+        public virtual ApplicationUser User { get; set; }
+    }
+}
